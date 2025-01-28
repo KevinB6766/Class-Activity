f8d952b70e0b3d07eda3318a48613625dea4150c
KevinB6766 <tuu21653@temple.edu>
2025-01-28 12:14:08 -0500

    Added interactivity to Click Me button

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,14 +1,32 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
+import android.view.View.OnClickListener
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    //    private lateinit var clickMeButton: Button
+//can do away with the above it is only used once
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (findViewById(R.id.button) as Button).setOnClickListener {
+            (it as Button).text = "Button clicked!"
+        }
+
+//       findViewById(R.id.button).setOnclickLIstener { view: View ->
+//           (view as Button).text = "Button clicked!"
+//       }
+//        val ocl = object: OnClickListener {
+//            overrider fun onClick(p0: View?) {
+//                (p0 as Button).text = "Button clicked!"e
+//            }
+//        }
     }
 }
